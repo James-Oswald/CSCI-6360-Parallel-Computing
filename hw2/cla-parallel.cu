d9b9cdbf915a321d4fd300987760a5251772fbc1
James Oswald <jtoswald@albany.edu>
2022-02-21 04:38:34 -0500

    wrote latest version

--- hw2/cla-parallel.cu
+++ hw2/cla-parallel.cu
@@ -19,7 +19,7 @@
 #define nsupersections nsections/block_size
 #define nsupersupersections nsupersections/block_size
 
-//Global definitions of the various arrays used in steps for easy access
+//pointers to device memory
 int* gi;
 int* pi; 
 int* ci;
@@ -44,7 +44,8 @@ int* dsumi;
 int* dbin1;
 int* dbin2;
 
-int sumi[bits] = {0}; //Change this back to be of size bits
+//host side
+int sumi[bits] = {0};
 int sumrca[bits] = {0};
 
 //Integer array of inputs in binary form
@@ -72,44 +73,26 @@ void read_input(int size){
 	free(in2);
 }
 
-
+//This function computes the gs and ps for all bits
 __global__ void compute_gp(const int* b1, const int* b2, int* g, int* p){
 	size_t i = blockDim.x * blockIdx.x + threadIdx.x;
-	if(i>=bits) return;
+	if(i>=bits) 
+		return; //Throw out any aditional threads 
 	g[i] = b1[i] & b2[i];
 	p[i] = b1[i] | b2[i];
 }
 
-/*__global__ void compute_g(int* gGroup, int* pGroup){
-	size_t i = blockDim.x * blockIdx.x + threadIdx.x;
-	for(int ii = block_size-1; ii > i; ii--)
-		gGroup[i] &= pGroup[ii]; //grabs the p_i terms and multiplies it with the previously multiplied stuff (or the g_i term if first round)
-	__syncthreads();
-	if(i < 16){
-		gGroup[i] |= gGroup[i + 16];
-		gGroup[i] |= gGroup[i + 8];
-		gGroup[i] |= gGroup[i + 4];
-		gGroup[i] |= gGroup[i + 1];
-	}
-}
-
-__global__ void compute_p(int* pGroup){
-	size_t i = blockDim.x * blockIdx.x + threadIdx.x;
-	if(i < 16){
-		pGroup[i] |= pGroup[i + 16];
-		pGroup[i] |= pGroup[i + 8];
-		pGroup[i] |= pGroup[i + 4];
-		pGroup[i] |= pGroup[i + 1];
-	}
-}*/
-
+//This is a generic kernal to compute the generates and propagates at any level 
+//nchunks is the number of bits in each section, subchunkg and subchunkp are the gs and ps
+//of the previous levels. chunkg and chunkp are the gs and ps computed from from block_size subchunk gs and ps. 
 template<int nchunks>
 __global__ void compute_chunk_gp(const int* subchunkg, const int* subchunkp, int* chunkg, int* chunkp){
 	size_t i = blockDim.x * blockIdx.x + threadIdx.x;
-	if(i>=nchunks) return;
+	if(i>=nchunks) 
+		return; //Throw out any aditional threads 
 	int start = i*block_size;
-	const int* gGroup = subchunkg+start; 				//pointer to the group of 32 g's this thread is reducing
-	const int* pGroup = subchunkp+start; 	    //pointer to the group of 32 p's this thread is reducing
+	const int* gGroup = subchunkg+start; 	//pointer to the group of 32 g's this thread is reducing
+	const int* pGroup = subchunkp+start; 	//pointer to the group of 32 p's this thread is reducing
 	
 	//computing the generate
 	int sum = 0;
@@ -128,6 +111,10 @@ __global__ void compute_chunk_gp(const int* subchunkg, const int* subchunkp, int
 	chunkp[i] = mult;
 }
 
+
+//This is a generic kernal to compute carries at any level.
+//It takes the list of generates and list of propagates of the current level and the carries of one level up
+//and computes the carries of the current chunk (subchunkc). nchunks is a generic paramater that 
 template<int nchunks>
 __global__ void compute_chunk_carry(const int* subchunkg, const int* subchunkp, const int* chunkc, int* subchunkc){
 	size_t i = blockDim.x * blockIdx.x + threadIdx.x;
@@ -146,14 +133,8 @@ __global__ void compute_sum(const int* b1, const int* b2, const int* c, int* sum
 	sum[i] = b1[i] ^ b2[i] ^ carry;
 }
 
-void cringePrint(int* thing, int size, const char* frmt = "%s\n"){
-	char* thingStr = int_to_string(thing, size);
-	char* rev = revbinary_to_hex(thingStr, size);
-	printf(frmt, rev);
-}
-
-
 void cla(){
+	//Allocate all of the GPU memory we're going to use
 	cudaMallocManaged((void**)&gi, bits*sizeof(int));
 	cudaMallocManaged((void**)&pi, bits*sizeof(int));
 	cudaMallocManaged((void**)&ci, bits*sizeof(int));
@@ -169,143 +150,55 @@ void cla(){
 	cudaMallocManaged((void**)&sssgm, nsupersupersections*sizeof(int));
 	cudaMallocManaged((void**)&ssspm, nsupersupersections*sizeof(int));
 	cudaMallocManaged((void**)&ssscm, nsupersupersections*sizeof(int));
-	const int nthreads = 32;
+
+	const int nthreads = 32; //The number of threads used, vary this for preformance report
+
+	//Compute top level gs and ps
 	compute_gp<<<bits,nthreads>>>(dbin1, dbin2, gi, pi);
+
+	//Compute gps for each layer
 	compute_chunk_gp<ngroups><<<ngroups,nthreads>>>(gi, pi, ggj, gpj);
 	compute_chunk_gp<nsections><<<nsections,nthreads>>>(ggj, gpj, sgk, spk);
 	compute_chunk_gp<nsupersections><<<nsupersections,nthreads>>>(sgk, spk, ssgl, sspl);
 	compute_chunk_gp<nsupersupersections><<<nsupersupersections,nthreads>>>(ssgl, sspl, sssgm, ssspm);
+	
+	//Compute carries for each layer	
 	compute_chunk_carry<1><<<1,nthreads>>>(sssgm, ssspm, nullptr, ssscm);
 	compute_chunk_carry<nsupersupersections><<<nsupersupersections,nthreads>>>(ssgl, sspl, ssscm, sscl);
 	compute_chunk_carry<nsupersections><<<nsupersections,nthreads>>>(sgk, spk, sscl, sck);
 	compute_chunk_carry<nsections><<<nsections,nthreads>>>(ggj, gpj, sck, gcj);
 	compute_chunk_carry<ngroups><<<ngroups,nthreads>>>(gi, pi, gcj, ci);
+
+	//finally compute the sum
 	compute_sum<<<bits,nthreads>>>(dbin1, dbin2, ci, dsumi);
+	
+	//ensure that all the values have been written to dsumi via a device sync
 	cudaDeviceSynchronize();
-	//int hsumi[bits];
-	//cudaMemcpy(hsumi, dsumi, bits*sizeof(int), cudaMemcpyDeviceToHost);
-	//cringePrint(hsumi, bits, "%.20s\n");
 }
 
-void ripple_carry_adder(const int size){
+//This is the built in RCA computation
+void ripple_carry_adder(){
 	int clast=0, cnext=0;
-	for(int i = 0; i < size; i++){
+	for(int i = 0; i < bits; i++){
 		cnext = (bin1[i] & bin2[i]) | ((bin1[i] | bin2[i]) & clast);
 		sumrca[i] = bin1[i] ^ bin2[i] ^ clast;
 		clast = cnext;
 	}
 }
 
-void check_cla_rca(const int size){
-	for(int i = 0; i < size; i++){
+//This is the built in RCA comparison
+void check_cla_rca(){
+	for(int i = 0; i < bits; i++){
 		if( sumrca[i] != sumi[i]){
 			printf("Check: Found sumrca[%d] = %d, not equal to sumi[%d] = %d - stopping check here!\n",
 				i, sumrca[i], i, sumi[i]);
-			//printf("bin1[%d] = %d, bin2[%d]=%d, gi[%d]=%d, pi[%d]=%d, ci[%d]=%d, ci[%d]=%d\n",
-			//	i, bin1[i], i, bin2[i], i, gi[i], i, pi[i], i, ci[i], i-1, ci[i-1]);
+			printf("bin1[%d] = %d, bin2[%d]=%d, gi[%d]=%d, pi[%d]=%d, ci[%d]=%d, ci[%d]=%d\n",
+				i, bin1[i], i, bin2[i], i, gi[i], i, pi[i], i, ci[i], i-1, ci[i-1]);
 			return;
 		}
 	}
 	printf("Check Complete: CLA and RCA are equal\n");
 }
-
-/*
-void babyCLA(){	
-
-	/*cudaMallocManaged((void**)&ssgl, nsupersections*sizeof(int));
-	cudaMallocManaged((void**)&sspl, nsupersections*sizeof(int));
-	cudaMallocManaged((void**)&sscl, nsupersections*sizeof(int));
-	cudaMallocManaged((void**)&sssgm, nsupersupersections*sizeof(int));
-	cudaMallocManaged((void**)&ssspm, nsupersupersections*sizeof(int));
-	cudaMallocManaged((void**)&ssscm, nsupersupersections*sizeof(int));
-	const int nthreads = 1;
-	
-	compute_gp<<<nsupersections,nthreads>>>(dbin1, dbin2, ssgl, sspl);
-	/*cudaDeviceSynchronize();
-	int hssgl[nsupersections];
-	int hsspl[nsupersections];
-	cudaMemcpy(hssgl, ssgl, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(hsspl, sspl, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	cringePrint(hssgl, nsupersections, "G1: %s\n");
-	cringePrint(hsspl, nsupersections, "P1: %s\n");
-	
-	compute_chunk_gp<<<nsupersupersections, nthreads>>>(nsupersupersections, ssgl, sspl, sssgm, ssspm);
-	/*cudaDeviceSynchronize();
-	int hsssgm[nsupersupersections];
-	int hssspm[nsupersupersections];
-	cudaMemcpy(hsssgm, sssgm, nsupersupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(hssspm, ssspm, nsupersupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	cringePrint(hsssgm, nsupersupersections, "G2: %s\n");
-	cringePrint(hssspm, nsupersupersections, "P2: %s\n");
-	/*cudaMemcpy(hssgl, ssgl, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(hsspl, sspl, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	cringePrint(hssgl, nsupersections, "G1: %s\n");
-	cringePrint(hsspl, nsupersections, "P1: %s\n");
-	
-	compute_chunk_carry<<<1,nthreads>>>(1, sssgm, ssspm, nullptr, ssscm);
-	/*cudaDeviceSynchronize();
-	int hssscm[nsupersupersections];
-	cudaMemcpy(hssscm, ssscm, nsupersupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	cringePrint(hssscm, nsupersupersections, "C1: %s\n");
-	
-	//cudaMemcpy(hssgl, ssgl, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	//cudaMemcpy(hsspl, sspl, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	//cringePrint(hssgl, nsupersections, "G1: %s\n");
-	//cringePrint(hsspl, nsupersections, "P1: %s\n");
-
-	compute_chunk_carry<<<nsupersupersections,nthreads>>>(nsupersupersections, ssgl, sspl, ssscm, sscl);
-	/*cudaDeviceSynchronize();
-	int hsscl[nsupersections];
-	cudaMemcpy(hsscl, sscl, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	cringePrint(hsscl, nsupersections, "C2: %s\n");
-
-	//int hbin1[nsupersections];
-	//int hbin2[nsupersections];
-	//cudaMemcpy(hbin1, dbin1, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	//cudaMemcpy(hbin2, dbin2, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	//cringePrint(hbin1, nsupersections, "B1: %s\n");
-	//cringePrint(hbin2, nsupersections, "B2: %s\n");
-
-	compute_sum<<<nsupersections,nthreads>>>(dbin1, dbin2, sscl, dsumi);
-	cudaDeviceSynchronize();
-
-	/*cudaDeviceSynchronize();
-	int hsumi[nsupersections];
-	cudaMemcpy(hsumi, dsumi, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	cringePrint(hsumi, nsupersections, "S0: %s\n");
-}*/
-
-/*
-int main(){
-	int size = 32*32;
-
-	char* hex1 = generate_random_hex(size);
-	char* hex2 = generate_random_hex(size);
-	//read_input(size);
-	char* hexa = prepend_non_sig_zero(hex1);
-	char* hexb = prepend_non_sig_zero(hex2);
-	bin1 = gen_formated_binary_from_hex(hexa);
-	bin2 = gen_formated_binary_from_hex(hexb);
-	cudaMallocManaged((void**)&dbin1, nsupersections*sizeof(int));
-	cudaMallocManaged((void**)&dbin2, nsupersections*sizeof(int));
-	cudaMallocManaged((void**)&dsumi, nsupersections*sizeof(int));
-	cudaMallocManaged((void**)&ssgl, nsupersections*sizeof(int));
-	cudaMallocManaged((void**)&sspl, nsupersections*sizeof(int));
-	cudaMallocManaged((void**)&sscl, nsupersections*sizeof(int));
-	cudaMallocManaged((void**)&sssgm, nsupersupersections*sizeof(int));
-	cudaMallocManaged((void**)&ssspm, nsupersupersections*sizeof(int));
-	cudaMallocManaged((void**)&ssscm, nsupersupersections*sizeof(int));
-	cudaMemcpy(dbin1, bin1, nsupersections*sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(dbin2, bin2, nsupersections*sizeof(int), cudaMemcpyHostToDevice);
-	babyCLA();
-	cudaMemcpy(sumi, dsumi, nsupersections*sizeof(int), cudaMemcpyDeviceToHost);
-	ripple_carry_adder(nsupersections);
-	check_cla_rca(nsupersections);
-
-	cringePrint(sumi, nsupersections);
-	cringePrint(sumrca, nsupersections);
-}*/
-
 
 int main(int argc, char *argv[]){
 	int randomGenerateFlag = 1;
@@ -344,9 +237,12 @@ int main(int argc, char *argv[]){
 	bin1 = gen_formated_binary_from_hex(hexa);
 	bin2 = gen_formated_binary_from_hex(hexb);
 
+	//Allocate device memory for the inputs and outputs
 	cudaMallocManaged((void**)&dbin1, bits*sizeof(int));
 	cudaMallocManaged((void**)&dbin2, bits*sizeof(int));
 	cudaMallocManaged((void**)&dsumi, bits*sizeof(int));
+
+	//Copy inputs over to the device
 	cudaMemcpy(dbin1, bin1, bits*sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(dbin2, bin2, bits*sizeof(int), cudaMemcpyHostToDevice);
 	
@@ -354,16 +250,17 @@ int main(int argc, char *argv[]){
 	cla();
 	end_time = clock_now();
 
+	//Copy outputs back client side for checking
 	cudaMemcpy(sumi, dsumi, bits*sizeof(int), cudaMemcpyDeviceToHost);
 	printf("CLA Completed in %llu cycles\n", (end_time - start_time));
 
 	start_time = clock_now();
-	ripple_carry_adder(bits);
+	ripple_carry_adder();
 	end_time = clock_now();
 
 	printf("RCA Completed in %llu cycles\n", (end_time - start_time));
 
-	check_cla_rca(bits);
+	check_cla_rca();
 
 	if( verbose==1 ){
 		int2str_result = int_to_string(sumi,bits);
