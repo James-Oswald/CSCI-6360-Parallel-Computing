fe37b35e7d04be31aaa7bdfcb149cfe013f9e5bb
James Oswald <jtoswald@albany.edu>
2022-02-28 01:40:54 -0500

    added time calculations

--- Assignment3/assignment3.c
+++ Assignment3/assignment3.c
@@ -4,6 +4,9 @@
 #include<mpi.h>
 #include"clockcycle.h"
 
+#define numbersToSum 2<<20
+#define clock_frequency 512000000
+
 int MPI_P2P_reduce(const void *sendbuf, void *recvbuf, int count,
                     MPI_Datatype datatype, MPI_Op op, int root,
                     MPI_Comm comm){
@@ -16,20 +19,22 @@ int MPI_P2P_reduce(const void *sendbuf, void *recvbuf, int count,
     int rank, comSize;
     MPI_Comm_size(comm, &comSize);
     MPI_Comm_rank(comm, &rank);
+
+    //comSize is a power of 2, MPI_P2P_reduce builds a binary tree and must have a power of 2 number of leaves
+    //for the reduction scheme specified in the assignment instructions
+    assert((comSize & (comSize - 1)) == 0); 
+
     int64_t localSum = 0;
     for(int i = 0; i < count; i++)
         localSum += localBuffer[i];
-    
-    printf("%d:%lu\n", rank, localSum);
     MPI_Barrier(comm); //Wait untill all local sums have been computed
 
     //Pairwise reduce
     MPI_Request request;
     for(int i = 1; i < comSize; i *= 2){
-        if(rank%i==0){
-            if(rank%(2*i)==0){
+        if(rank%i==0){          //On level n, only 2^n's will send or recive              
+            if(rank%(2*i)==0){  //On level, n every rank 2*(2^n) appart will recive from the rank Rank 2^n ahead
                 uint64_t incomingSum;
-                printf("%d: %d, %d \n", rank, rank%(2*i), rank+i);
                 MPI_Irecv(&incomingSum, 1, datatype, rank+i, MPI_ANY_TAG, comm, &request);
                 MPI_Wait(&request, MPI_STATUS_IGNORE);
                 localSum += incomingSum;
@@ -54,19 +59,33 @@ int main(int argc, char** argv){
     int rank, comSize;
     MPI_Comm_size(MPI_COMM_WORLD, &comSize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    assert((comSize & (comSize - 1)) == 0); //comSize is a power of 2, MPI_P2P_reduce wont work otherwise
     
-    const int sizeToSum = 2<<20;
-    size_t localArraySize = (sizeToSum/comSize);
+    size_t localArraySize = (numbersToSum/comSize);
     int* localArray = malloc(localArraySize*sizeof(int));
     for(int i = 0; i < localArraySize; i++)
         localArray[i] = rank*localArraySize + i;
 
     uint64_t result;
-    MPI_P2P_reduce(localArray, &result, localArraySize, MPI_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    //My MPI_P2P_reduce reduction
+    uint64_t startTime = clock_now();
+    MPI_P2P_reduce(localArray, &result, localArraySize, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    uint64_t endTime = clock_now();
+    double time_in_secs = ((double)(endTime - startTime)) / clock_frequency;
     if(rank == 0)
-        printf("%lu\n", result);
+        printf("MPI_P2P_reduce\nResult:%lu\nTime:%lf\n\n", result, time_in_secs);
+
+    //MPI_Reduce reduction
+    startTime = clock_now();
+    uint64_t localSum = 0;  //Sum local array
+    for(int i = 0; i < localArraySize; i++)
+        localSum += localArray[i];
+    MPI_Reduce(&localSum, &result, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD); //reduce to single elm
+    endTime = clock_now();
+    time_in_secs = ((double)(endTime - startTime)) / clock_frequency;
+    if(rank == 0)
+        printf("MPI_reduce\nResult:%lu\nTime:%lf\n", result, time_in_secs);
+    
 
     free(localArray);
     MPI_Finalize();
