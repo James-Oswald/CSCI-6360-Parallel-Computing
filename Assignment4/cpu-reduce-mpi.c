70e79068fb76e520065a40a9e4ad06ba986016ae
James Oswald <jtoswald@albany.edu>
2022-03-03 13:37:26 -0500

    added CPU reduce for assignment 4

---
+++ Assignment4/cpu-reduce-mpi.c
@@ -0,0 +1,44 @@
+
+#include<mpi.h>
+#include<assert.h>
+#include<stdio.h>
+#include"clockcycle.h"
+
+#define numDoubles 1610612736
+#define clockFrequency 512000000
+
+int main(int argc, char** argv){
+
+    //Setup
+    MPI_Init(&argc, &argv);
+    int worldSize, rank; 
+    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    //Initialize the array of doubles
+    assert(numDoubles%worldSize == 0);
+    size_t localArraySize = numDoubles/worldSize;
+    double* localArray = (double*)malloc(localArraySize*sizeof(double));
+    for(size_t i = 0; i < localArraySize; i++)
+        localArray[i] = rank*localArraySize + i;
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //Preform Local Sum
+    uint64_t startTime = clock_now();
+    double localSum = 0;
+    for(int i = 0; i < localArraySize; i++)
+        localSum += localArray[i];
+    //global sum with MPI reduce
+    double globalSum;
+    MPI_Reduce(&localSum, &globalSum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    uint64_t endTime = clock_now();
+
+    //Print result
+    double time_in_secs = ((double)(endTime - startTime))/clockFrequency;
+    if(rank == 0)
+        printf("%lf %lf\n", globalSum, time_in_secs);
+
+    free(localArray);
+    MPI_Finalize();
+}
+
